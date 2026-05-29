3a0d8949aa20b62fcfe7cc71a57d3156660fe722
augusto9aslas-wq <augusto9aslas@gmail.com>
2026-05-28 22:27:11 -0300

    arrumar dtos

---
+++ backend/src/main/java/br/com/minerva/minerva/dto/MateriaRequest.java
@@ -0,0 +1,19 @@
+package br.com.minerva.minerva.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MateriaRequest {
+
+	@NotBlank(message = "O nome da matéria é obrigatório")
+	private String nome;
+
+	@NotNull(message = "O ID do curso é obrigatório")
+	private Long cursoId;
+}
